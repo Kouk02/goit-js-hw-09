339038282a88bd053ac24d9354c0af86db0167b9
Kouk02 <kouk20020703@gmail.com>
2023-10-29 03:45:23 +0300

    update task-01 1.0

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,8 +1,6 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`; //рандомний колір
 }
-
- 
 const startButton = document.querySelector('[data-start]');//! Отримуємо посилання на кнопки та <body>
 const stopButton = document.querySelector('[data-stop]');
 const body = document.body;
@@ -12,7 +10,6 @@ let intervalId;
 
 
 startButton.addEventListener('click', function() {//!кнопка "Start"
-  
   startButton.disabled = true;// вкл "Start"
   stopButton.disabled = false;// викл "Stop"
 
@@ -21,5 +18,9 @@ startButton.addEventListener('click', function() {//!кнопка "Start"
     body.style.backgroundColor = getRandomHexColor();
   }, 1000);
 });
-
-
+stopButton.addEventListener('click', function() {//! Обробник події для кнопки "Stop"
+  // Вимикаємо кнопку "Stop" і активуємо "Start"
+  stopButton.disabled = true; //викл "Stop"
+  startButton.disabled = false; //вкл "Start"
+  clearInterval(intervalId); //! Зупиняємо таймер
+});
