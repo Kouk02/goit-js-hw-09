130bde6f10e103fa1babdfdded5871ce78aa34e7
Kouk02 <kouk20020703@gmail.com>
2023-10-29 03:43:09 +0300

    create task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`; //рандомний колір
+}
+
+ 
+const startButton = document.querySelector('[data-start]');//! Отримуємо посилання на кнопки та <body>
+const stopButton = document.querySelector('[data-stop]');
+const body = document.body;
+
+//!Змінна для збереження ідентифікатора
+let intervalId; 
+
+
+startButton.addEventListener('click', function() {//!кнопка "Start"
+  
+  startButton.disabled = true;// вкл "Start"
+  stopButton.disabled = false;// викл "Stop"
+
+  //! Запускаємо таймер який змінює колір фону кожну секунду
+  intervalId = setInterval(function() {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
 
