ae4e8459fc2c28740bc9fbcb456a3db8f187a209
Kouk02 <kouk20020703@gmail.com>
2023-10-29 04:24:08 +0200

    fix tast-03 1.0

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,43 +1,42 @@
 import Notiflix from 'notiflix';
-
-document.querySelector('.form').addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    const delay = parseInt(document.querySelector('input[name="delay"]').value);
-    const step = parseInt(document.querySelector('input[name="step"]').value);
-    const amount = parseInt(document.querySelector('input[name="amount"]').value);
-
-    if (isNaN(delay) || isNaN(step) || isNaN(amount) || delay < 0 || step <= 0 || amount <= 0) {
-        // Input validation
-        Notiflix.Notify.Failure('Please enter valid values');
-        return;
-    }
-
-    for (let i = 1; i <= amount; i++) {
-        const position = i;
-        const currentDelay = delay + (i - 1) * step;
-
-        createPromise(position, currentDelay)
-            .then(({ position, delay }) => {
-                Notiflix.Notify.Success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-            })
-            .catch(({ position, delay }) => {
-                Notiflix.Notify.Failure(`❌ Rejected promise ${position} in ${delay}ms`);
-            });
-    }
-});
+const promiseGenerator = document.querySelector('.form');
 
 function createPromise(position, delay) {
-    return new Promise((resolve, reject) => {
-        const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
 
-        setTimeout(() => {
-            if (shouldResolve) {
-                resolve({ position, delay });
-            } else {
-                reject({ position, delay });
-            }
-        }, delay);
+function handlePromise(position, delay) {
+  createPromise(position, delay)
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
     });
 }
 
+promiseGenerator.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = parseInt(promiseGenerator.elements.delay.value);
+  const step = parseInt(promiseGenerator.elements.step.value);
+  const amount = parseInt(promiseGenerator.elements.amount.value);
+
+  if (delay < 0 || step < 0 || amount < 0) {
+    Notiflix.Notify.warning('⚠️ The form should contain only positive values');
+  } else {
+    for (let position = 0; position < amount; position++) {
+      const currentDelay = delay + position * step;
+      handlePromise(position + 1, currentDelay);
+    }
+  }
+});
